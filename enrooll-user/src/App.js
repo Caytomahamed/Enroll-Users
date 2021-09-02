485d56befb4a4e2adb34b8097a330100ad696ddb
Aitoodeveloper <aidarous1143@gmail.com>
2021-09-02 15:02:49 +0300

    added validation

--- enrooll-user/src/App.js
+++ enrooll-user/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Form from './Component/Form';
+import User from './Component/Users';
 function App() {
   return (
     <div className="App App-header">
       <Form/>
+      <User/>
     </div>
   );
 }
